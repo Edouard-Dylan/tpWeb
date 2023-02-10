760490f89108f44d5f7b3a8bc54ff0ca81a51d4f
Eddy <eddy.hazard@etudiant.univ-rennes1.fr>
2023-02-10 10:03:01 +0100

    Q1-Q8

--- interaction.js
+++ interaction.js
@@ -1,23 +1,49 @@
+// La création d'un Dnd requière un canvas et un interacteur. 
+// L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ. 
+function DnD(canvas, interactor) { 
+  // Définir ici les attributs de la 'classe' 
+  this.xInit = 0; 
+  this.yInit = 0; 
+  this.xFinal = 0; 
+  this.yFinal = 0; 
+  this.pressionFirst = false;
+  
+  // Developper les 3 fonctions gérant les événements 
+  function maFctGérantLaPression(evt){ 
+    let xy = getMousePosition(this,evt);
+    this.xInit = xy.x;
+    this.yInit = xy.y;
+    this.pressionFirst = true;
+    console.log("Pression x:"+this.xInit+" y:"+this.yInit);
+  } 
+  
+  function maFctGérantLeDéplacement(evt){ 
+    if(this.pressionFirst){
+      let xy = getMousePosition(this,evt);
+      this.xFinal = xy.x;
+      this.yFinal = xy.y;
+      console.log("Déplacement x:"+this.xFinal+" y:"+this.yFinal);
+    }
+  } 
 
-// La création d'un Dnd requière un canvas et un interacteur.
-// L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
-function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
-
-	// Developper les 3 fonctions gérant les événements
-
-	// Associer les fonctions précédentes aux évènements du canvas.
+  function maFctGérantLeRelâchement(evt){
+    if(this.pressionFirst){
+      let xy = getMousePosition(this,evt);
+      this.xFinal = xy.x;
+      this.yFinal = xy.y;
+      this.pressionFirst = false;
+      console.log("Relachement x:"+this.xFinal+" y:"+this.yFinal);
+    }
+  }
+  
+  // Associer les fonctions précédentes aux évènements du canvas. 
+  canvas.addEventListener('mousedown', maFctGérantLaPression, false); 
+  canvas.addEventListener('mousemove', maFctGérantLeDéplacement, false); 
+  canvas.addEventListener('mouseup', maFctGérantLeRelâchement, false); 
+}; 
+  
+// Place le point de l'événement evt relativement à la position du canvas. 
+function getMousePosition(canvas, evt) { 
+  var rect = canvas.getBoundingClientRect(); 
+  return { x: evt.clientX - rect.left, y: evt.clientY - rect.top }; 
 };
-
-
-// Place le point de l'événement evt relativement à la position du canvas.
-function getMousePosition(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
-  return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
-  };
-};
-
-
-
--- model.js
+++ model.js
@@ -1,3 +1,75 @@
-
 // Implémenter ici les 4 classes du modèle.
+function Drawing() {
+    //Arttributs
+    this.shapeList = new Array();
+
+    this.getForms = function(){
+        return this.shapeList;
+    }
+}
+
+function Shape(thickness,color){
+    //Attributs
+    this.color = color;
+    this.thickness = thickness;
+
+    this.getColor = function(){
+        return this.color;
+    }
+
+    this.getThickness = function(){
+        return this.thickness;
+    }
+}
+
+function Rectangle(x, y, width, height, thickness, color) {
+    //Attributs
+    Shape.call(this,thickness,color);
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+
+    this.getInitX = function(){
+      return this.x;
+    }
+  
+    this.getInitY = function(){
+      return this.y;
+    }
+  
+    this.getFinalX = function(){
+      return this.x+this.width;
+    }
+  
+    this.getFinalY = function(){
+      return this.y+this.height;
+    }
+}
+
+function Line(x1, y1, x2, y2, thickness, color) {
+    //Attributs
+    Shape.call(this,thickness,color);
+    this.x1=x1;
+    this.y1=y1;
+    this.x2=x2;
+    this.y2=y2;
+
+    this.getInitX = function(){
+      return this.x1;
+    }
+
+    this.getInitY = function(){
+      return this.y1;
+    }
+
+    this.getFinalX = function(){
+      return this.x2;
+    }
+
+    this.getFinalY = function(){
+      return this.y2;
+    }
+}
+
 // N'oubliez pas l'héritage !
